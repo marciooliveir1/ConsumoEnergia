11860b4e3b5f1e16eba84c625ca7ce770dd21614
Marcio de Oliveira <marciooliveir1@gmail.com>
2021-03-01 14:00:21 -0300

    Add files via upload

---
+++ Energia.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    $classe = $_GET["classe"];
+    $gasto = $_GET["kw"];
+    $imp = 0.03;
+    if($classe == ""){
+        echo("Informa a classe!");
+    }else
+        if($gasto == ""){
+            echo("Informa o conssumo");
+        }  else
+    if($classe == "a"){
+        $t = 0.70;
+        $a = $gasto * $t;
+        $a = ($a * $imp + $a);
+        echo("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+            <table border='12'><tr><td><body bgcolor='teal'</body><b><h4>
+                O valor da sua conta é de R$ $a</h4></b>
+                </td></tr></table>");
+    }else
+        if($classe == "b"){
+            $t = 0.40;
+            $a = $gasto * $t;
+            $a = ($a * $imp + $a);
+            echo("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+            <table border='12'><tr><td><body bgcolor='teal'</body><b><h4>
+                O valor da sua conta é de R$ $a</h4></b>
+                </td></tr></table>");
+        }else
+            if($clase == "c"){
+                 $t = 0.30;
+            $a = $gasto * $t;
+            $a = ($a * $imp + $a);
+            echo("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+            <table border='12'><tr><td><body bgcolor='teal'</body><b><h4>
+                O valor da sua conta é de R$ $a</h4></b>
+                </td></tr></table>");
+            }
+?>
